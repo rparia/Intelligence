a2fe67d98841214d7ffa2c61216decef555278d0
rparia <rpearsall@aria-core.tech>
2026-02-05 11:45:27 -0800

    Add execute function to run shell commands

--- world/shell.py
+++ world/shell.py
@@ -1,1 +1,21 @@
 
+import subprocess
+import time
+
+def execute(command: str):
+    start = time.time()
+    proc = subprocess.Popen(
+        command,
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        text=True
+    )
+    stdout, _ = proc.communicate()
+    end = time.time()
+
+    return {
+        "stdout": stdout.strip(),
+        "exit_code": proc.returncode,
+        "duration": end - start
+    }
