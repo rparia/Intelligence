30674d659775017d211d6b2a6a41b233c3f5d185
rparia <rpearsall@aria-core.tech>
2026-02-05 11:47:17 -0800

    Add EmotionalTrace class for managing emotional state

--- core/emotion.py
+++ core/emotion.py
@@ -1,1 +1,11 @@
 
+class EmotionalTrace:
+    def __init__(self, decay=0.95):
+        self.rolling = 0.0
+        self.decay = decay
+
+    def update(self, surprise):
+        self.rolling = self.decay * self.rolling + (1 - self.decay) * surprise
+
+    def risk_bias(self):
+        return min(1.0, self.rolling / 50.0)
